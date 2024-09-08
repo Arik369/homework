5e57c296776acc579095e0d95bf45d953598c5e9
Artur <arik2011@bk.ru>
2024-09-08 20:11:57 +0300

    08/09-2

---
+++ peremenie.py
@@ -0,0 +1,4 @@
+number_of_completed_tasks = "всего задач:12"
+number_of_hours_spent = "затрачено часов:1.5"
+course_name = "Python"
+print(course_name,number_of_completed_tasks,number_of_hours_spent,'среднее выполнение ',(1.5 / 12),'часа')

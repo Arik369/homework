f750f3af754769ed8c152ecbc270375b1d6ce359
Artur <arik2011@bk.ru>
2024-09-19 20:36:47 +0300

    19/09

---
+++ module_1.4.py
@@ -0,0 +1,9 @@
+my_string = input('urban')
+name = len(my_string)
+print(name)
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_', ''))
+name_1 = 'my_string'
+print(name_1[0])
+print(name_1[-1])

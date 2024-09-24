b6e99845d4f99246b76eda52689579a6f6831570
Artur <arik2011@bk.ru>
2024-09-24 16:51:07 +0300

    24/09-3

--- module_1.5.py
+++ module_1.5.py
@@ -1,8 +1,7 @@
 immutable_var = 1,2,"a","b"
 print(immutable_var)
-#immutable_var = [1] = 3 ошибка, потому что не поддерживается по элементам
+#immutable_var = [1] = 3 ошибка, потому что кортежи не поддерживают изменения по элементам
 #print(immutable_var)
-mutable_list = ([1,2,3],1)
+mutable_list = [1,2,3,'a','b']
 print(mutable_list)
-mutable_list[0][2] = 2
-print(mutable_list)
+print(mutable_list[0:3])

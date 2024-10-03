196991bdcb4930397e27fe5f1f68db68bcaa9301
Artur <arik2011@bk.ru>
2024-10-03 20:12:49 +0300

    03/10

---
+++ module_1.6.py
@@ -0,0 +1,20 @@
+my_dict = {'Artur':1983, 'Rita':2005, 'Veronika':2010}
+print(my_dict)
+print(my_dict['Artur'])
+my_dict.update({'Sasha':1989,
+                'Masha':1990})
+print(my_dict)
+f = my_dict.pop('Sasha')
+print(my_dict)
+print(f)
+print(my_dict)
+# множество
+my_set = {2002, 2003, 2004, 2002,'Artur',(5,6,7)}
+print(my_set)
+list=[2002,2003,2004,2002,'Artur',(5,6,7)]
+list=set(list)
+print(list.add(13))
+print(list.add(15))
+print(list)
+print(list.discard(2004))
+print(list)

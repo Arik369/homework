9a2f02c6fcfad97f6f5e90decad50892d3b8eceb
Artur <arik2011@bk.ru>
2024-10-30 14:23:06 +0300

    30-10-1

--- zadacha1.py
+++ zadacha1.py
@@ -6,5 +6,5 @@ students_list = sorted(list(students))
 grades_average_list = [(sum(grades[0]) / len(grades[0])),(sum(grades[1]) / len(grades[1])),(sum(grades[2]) / len(
 grades[2])),(sum(grades[3]) / len(grades[3])),(sum(grades[4]) / len(grades[4]))]
 
-average_grades_dict = dict(zip(students_list, grades_average_list))
-print(average_grades_dict)
+average_grades = dict(zip(students_list, grades_average_list))
+print(average_grades)
